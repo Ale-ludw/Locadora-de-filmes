086b1179062b76d08035b44d0866db9775b28645
Ale-ludw <alexsandrojacksomludwigvasco@gmail.com>
2025-02-06 20:55:29 -0300

    Adição: imagens padrões para acesso na inicialização

--- script.js
+++ script.js
@@ -1,4 +1,53 @@
-const filmes = JSON.parse(localStorage.getItem("filmes")) || [];
+const filmes = JSON.parse(localStorage.getItem("filmes")) || [{
+    titulo: "O Senhor dos Anéis: A Sociedade do Anel",
+    imagem: "https://br.web.img3.acsta.net/medias/nmedia/18/92/91/32/20224832.jpg", 
+    genero: "Fantasia"
+},
+{
+    titulo: "O Poderoso Chefão",
+    imagem: "https://ingresso-a.akamaihd.net/prd/img/movie/o-poderoso-chefao-50-anos/008274f7-a76d-484f-8b5b-1c639c2ce46b.jpg", 
+    genero: "Crime"
+},
+{
+    titulo: "Matrix",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQHcTCyMucK7uemubWm_RX8xcMSYKmz9fsWw&s", 
+    genero: "Ficção Científica"
+},
+{
+    titulo: "Titanic",
+    imagem: "https://upload.wikimedia.org/wikipedia/pt/2/22/Titanic_poster.jpg", 
+    genero: "Drama/Romance"
+},
+{
+    titulo: "Vingadores: Ultimato",
+    imagem: "https://br.web.img3.acsta.net/pictures/19/04/26/17/30/2428965.jpg", 
+    genero: "Ação"
+},
+{
+    titulo: "Forrest: O Contador de Histórias",
+    imagem: "https://upload.wikimedia.org/wikipedia/pt/thumb/c/c0/ForrestGumpPoster.jpg/240px-ForrestGumpPoster.jpg", 
+    genero: "Drama"
+},
+{
+    titulo: "O Rei Leão",
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfRsXIMEsvQTXiXBhPw2sRbB5u6FuFTUKG7g&s", 
+    genero: "Animação"
+},
+{
+    titulo: "Star Wars: O Império Contra-Ataca",
+    imagem: "https://m.media-amazon.com/images/M/MV5BY2ViMjFhMDMtNTA4Yi00NGJjLTk0ZTktNGVkZjJjMGY4ZTFkXkEyXkFqcGc@._V1_.jpg", 
+    genero: "Ficção Científica"
+},
+{
+    titulo: "Jurassic Park",
+    imagem: "https://preview.redd.it/xflrnkg1t9ae1.jpeg?width=640&crop=smart&auto=webp&s=f061e01798f35e775f3fe1d00e681947dc5f21bf", 
+    genero: "Aventura/Ficção Científica"
+},
+{
+    titulo: "O Lobo de Wall Street",
+    imagem: "O Lobo de Wall Street", 
+    genero: "Comédia/Drama"
+}];
 
 // Função para exibir os filmes com filtro de título
 function exibirFilmes(containerId, botoes = [], filtroTitulo = "", filtroGenero = "") {
